199c7dc349dee4a9a6ac7efdaf50df9b483962d1
ryoww <ryo.shimozawa.0121@gmail.com>
2024-12-04 00:24:42 +0900

    out_csv_hpc.pyはめちゃくちゃ冷えてコアがめちゃくちゃあるPC以外で実行するな

--- out_csv.py
+++ out_csv.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+import threading
+import queue
 from main import create_users, simulate_transmission, PRINT_MODE
 
 # パラメータ
@@ -8,23 +10,57 @@ rate = 24  # Mbps
 duration = 60  # seconds
 mode = 'a'  # Transmission mode
 n_values = [1, 10, 20, 30, 40, 50, 60, 70, 80]
-num_simulations = 10  # 各nでのシミュレーション回数
+num_simulations = 500  # 各nでのシミュレーション回数
+max_threads = 10  # 同時実行スレッド数の上限
 
 results = []
+results_lock = threading.Lock()  # 結果の保存用ロック
 
-def process_n(d, duration, rate, mode):
-    users = create_users(n)
-    data_transmitted = simulate_transmission(users, duration, rate, output_mode=PRINT_MODE[3], mode=mode)
-    return float(data_transmitted)
+# タスクキューの作成
+task_queue = queue.Queue()
 
-for simu in tqdm(range(num_simulations), leave=False):
-    res = []
-    for n in tqdm(n_values, desc='Number of Users', leave=False):
-        users = create_users(n)
-        data_transmitted = simulate_transmission(users, duration, rate, output_mode=PRINT_MODE[3], mode=mode)
-        res.append(float(data_transmitted))
-    results.append(res)
+# シミュレーションタスクをキューに追加
+for simu_index in range(num_simulations):
+    task_queue.put(simu_index)
 
+# プログレスバーの設定
+overall_pbar = tqdm(total=num_simulations, desc='全体の進行度', position=0)
+thread_pbars = {}  # スレッドIDとプログレスバーのマッピング
 
-# DataFrameの作成
-df_results = pd.DataFrame(np.array(results), columns=n_values).to_csv('./out/out.csv')
+# スレッドのワーカー関数
+def worker_thread(thread_id, position):
+    thread_pbar = tqdm(total=len(n_values), desc=f'スレッド{thread_id}', position=position, leave=True)
+    while not task_queue.empty():
+        try:
+            simu_index = task_queue.get_nowait()
+        except queue.Empty:
+            break
+        res = []
+        for n in n_values:
+            users = create_users(n)
+            data_transmitted = simulate_transmission(users, duration, rate, output_mode=PRINT_MODE[3], mode=mode)
+            res.append(float(data_transmitted))
+            thread_pbar.update(1)
+        with results_lock:
+            results.append(res)
+        overall_pbar.update(1)
+        thread_pbar.reset()
+        task_queue.task_done()
+    thread_pbar.close()
+
+# スレッドの作成と開始
+threads = []
+for i in range(max_threads):
+    t = threading.Thread(target=worker_thread, args=(i+1, i+1))
+    t.start()
+    threads.append(t)
+
+# 全てのタスクが完了するのを待機
+for t in threads:
+    t.join()
+
+overall_pbar.close()
+
+# DataFrameの作成と保存
+df_results = pd.DataFrame(np.array(results), columns=n_values)
+df_results.to_csv('./out/out.csv', index=False)
