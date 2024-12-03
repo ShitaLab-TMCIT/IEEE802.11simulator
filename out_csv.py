65466226603e30364196ae7cff13442feb29d188
ryoww <ryotaroshimozawa.0121@gmail.com>
2024-12-03 22:42:49 +0900

    update

--- out_csv.py
+++ out_csv.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
-from concurrent.futures import ThreadPoolExecutor, as_completed
 from main import create_users, simulate_transmission, PRINT_MODE
 
 # パラメータ
@@ -11,27 +10,21 @@ mode = 'a'  # Transmission mode
 n_values = [1, 10, 20, 30, 40, 50, 60, 70, 80]
 num_simulations = 10  # 各nでのシミュレーション回数
 
-# 並列処理を実行する関数
-def process_single_n(n):
-    users = create_users(n)
-    data_transmitted = simulate_transmission(users, duration, rate, output_mode=PRINT_MODE[3], mode=mode)
-    return n, float(data_transmitted)
-
-# 結果格納用リスト
 results = []
 
-# 並列処理の実行
-for simu in tqdm(range(num_simulations), desc="Simulations", leave=False):
-    with ThreadPoolExecutor() as executor:
-        # フューチャーオブジェクトを生成
-        futures = {executor.submit(process_single_n, n): n for n in n_values}
-        
-        # tqdmでn_valuesに対応した進捗バーを表示
-        res = []
-        for future in tqdm(as_completed(futures), total=len(n_values), desc=f"Simulation {simu + 1}", leave=False):
-            n, data_transmitted = future.result()
-            res.append(data_transmitted)
-        results.append(res)
+def process_n(d, duration, rate, mode):
+    users = create_users(n)
+    data_transmitted = simulate_transmission(users, duration, rate, output_mode=PRINT_MODE[3], mode=mode)
+    return float(data_transmitted)
+
+for simu in tqdm(range(num_simulations), leave=False):
+    res = []
+    for n in tqdm(n_values, desc='Number of Users', leave=False):
+        users = create_users(n)
+        data_transmitted = simulate_transmission(users, duration, rate, output_mode=PRINT_MODE[3], mode=mode)
+        res.append(float(data_transmitted))
+    results.append(res)
+
 
 # DataFrameの作成
 df_results = pd.DataFrame(np.array(results), columns=n_values).to_csv('./out/out.csv')
