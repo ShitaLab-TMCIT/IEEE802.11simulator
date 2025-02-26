7ba1469be0f17ea4a203f3c77084bc44483288e0
ryoww <ryotaroshimozawa.0121@gmail.com>
2025-02-26 15:38:17 +0900

    Add: coments

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # IEEE802.11simulator
 Network simulator for IEEE802.11 (Wi-Fi) using MATLAB
+
+
+- シミュレータ本体は`main.py`
+- 同条件複数回行う場合は`out_csv.py`で
+- CPUがめちゃつよで安全なPCを持っているのなら`out_csv_hpc.py`を使うと早い(13,14世代Intelで使わないこと)
+- 出力されたcsvの確認は`main.ipynb`で行う
+- `out_csv.py`と`main.ipynb`は`pip install -r requirements.txt`しないと実行できない
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+# 出力形式あんま使ってない
 PRINT_MODE = {
     0: "Only Collision",
     1: "ALL",
@@ -7,6 +8,7 @@ PRINT_MODE = {
     3: "No Output"
 }
 
+# モードによって辞書型で管理してる
 TRANS_MODE = {
     "a": {
         "SLOT_TIME": 9,
@@ -28,7 +30,7 @@ TRANS_MODE = {
 }
 
 
-
+# パケット作るときに使おうと思ったけど使ってない
 OFDM_DATA_BIT = {
     6: 24,
     9: 35,
@@ -40,6 +42,7 @@ OFDM_DATA_BIT = {
     54: 216
 }
 
+# ACKを送るのにかかる時間`./data/20100211_11a-mac-frame.pdf`にある資料から抜粋
 ACK_TIME = {
     6: 52,
     9: 44,
@@ -51,16 +54,18 @@ ACK_TIME = {
     54: 24
 }
 
-
+# Userクラス
 class User:
     def __init__(self, id, n=0, seed=None):
-        # if seed is not None:
-        #     random.seed(seed+id)
-
+        # ID
         self.id = id
+        # 再送回数
         self.num_re_trans = n
+        # スロット生成
         self.slots = self.calc_slots()
+        # 何回送信したか
         self.num_transmitted = 0
+        # 送信したデータ量
         self.data_transmitted = 0
 
     def calc_slots(self):
@@ -77,44 +82,48 @@ class User:
         self.slots = self.calc_slots()
 
 
+# パケット生成クラスを作ろうと思って辞めた
 class Packet:
     def __init__(self, mode, rate, level):
         self.PLCP_preamble = 16
 
+# Userリストを作成(現在は全く同じUserをnum_users分作成している)(今後Userごとに何かしらのパラメータを変更する場合,ここを変更する)
 def create_users(num_users, seed=None):
     return [User(id=i, seed=seed) for i in range(num_users)]
 
 
+# 送るデータ量と伝送レートから伝送時間を計算
 def calc_trans_time(data, rate):
     return data / (rate * 10**6)
 
 
+# バックオフ時間の計算
 def calc_cw_time(slots, mode):
     return slots * TRANS_MODE[mode]['SLOT_TIME'] * 10**(-6)
 
 
+# 任意のIFSの時間を持ってくる関数
 def calc_ifs_time(ifs, mode):
     return TRANS_MODE[mode][ifs] * 10**(-6)
 
-
+# シミュレーション関数
 def simulate_transmission(users: User, duration: int, rate, output_mode, mode):
     current_time = 0
     collision_count = 0
-    trans_data = 1472 * 8 # UDP
-    # trans_data = 1500 * 8 # IP
+    trans_data = 1472 * 8 # UDP level
+    # trans_data = 1500 * 8 # IP level
     total_data_transmitted = 0
     trans_num = 0
 
     preamble_time = 20 / (10**6)
     FCS_time = 4 * 8 / (rate * 10**6)
-    # ACK_time = 14 * 8 / (rate * 10**6)
     ACK_time = 28 / 10**6
     MAC_header_time = 24 * 8 / (rate * 10**6)
 
     cw_time_list = [(user.id, user.slots) for user in users]
     # print(cw_time_list)
 
-    nav_time = calc_ifs_time('SIFS', mode) + ACK_time + calc_ifs_time('DIFS', mode)
+    EIFS_time = calc_ifs_time('SIFS', mode) + ACK_time + calc_ifs_time('DIFS', mode)
     trans_time = calc_trans_time(trans_data, rate)
 
     while current_time < duration:
@@ -127,21 +136,20 @@ def simulate_transmission(users: User, duration: int, rate, output_mode, mode):
             collisions.append(min_user)
         collisions_ids = [user.id for user in collisions]
 
+
         # print(cw_time_list)
         # print('collisions_ids: ', collisions_ids)
 
-        # nav_time = calc_ifs_time('DIFS', mode) + (15 * TRANS_MODE[mode]['SLOT_TIME'] / 2) * 10**(-6)
         cw_time = calc_cw_time(min_user.slots, mode)
         min_slots = min_user.slots
 
-        # print('trans_time: ', trans_time, 'cw_time: ', cw_time, 'min_user_slots', min_slots)
-        # 衝突
+        # 衝突時処理
         if collisions_ids:
             collision_count += 1
 
-            # バックオフ + データ送信 + DIFS時間が今の時間を超えないなら
-            if (current_time + cw_time + preamble_time + MAC_header_time + trans_time + FCS_time + nav_time) < duration:
-                current_time += cw_time + preamble_time + MAC_header_time + trans_time + FCS_time + nav_time
+            # バックオフ + DIFS時間 + データ送信が今の時間を超えないなら
+            if (current_time + cw_time + preamble_time + MAC_header_time + trans_time + FCS_time + EIFS_time) < duration:
+                current_time += cw_time + preamble_time + MAC_header_time + trans_time + FCS_time + EIFS_time
 
                 if output_mode in [PRINT_MODE[0], PRINT_MODE[1]]:
 
@@ -151,9 +159,11 @@ def simulate_transmission(users: User, duration: int, rate, output_mode, mode):
                         print(f"User {user.id} waited {user.slots} slots before collision.")
 
                 for user in users:
+                    # 衝突Userだったら再送処理
                     if user in collisions:
                         user.re_transmit()
 
+                    # それ以外はslotsから最小slotsを引く
                     else:
                         user.slots -= min_slots
 
@@ -163,14 +173,14 @@ def simulate_transmission(users: User, duration: int, rate, output_mode, mode):
             else:
                 current_time = duration
 
-        # 成功
+        # 成功時処理
         else:
             trans_num += 1
             min_user.num_transmitted += 1
 
             # 送信時間が制限時間を超えないなら
-            if (current_time + cw_time + preamble_time + MAC_header_time + trans_time + FCS_time + nav_time) < duration:
-                current_time += cw_time + preamble_time + MAC_header_time + trans_time + FCS_time + nav_time
+            if (current_time + cw_time + preamble_time + MAC_header_time + trans_time + FCS_time + EIFS_time) < duration:
+                current_time += cw_time + preamble_time + MAC_header_time + trans_time + FCS_time + EIFS_time
                 min_user.data_transmitted += trans_data
                 total_data_transmitted += trans_data
 
@@ -183,6 +193,7 @@ def simulate_transmission(users: User, duration: int, rate, output_mode, mode):
                 
                 # print(f"remaining : {remaining_time}")
 
+                # 残り伝送時間内にデータフレームを送る時間があるなら`remaining_time`がプラス
                 if 0 < remaining_time:
                 
                     transmitted_data = remaining_time * rate * 10**6
@@ -199,17 +210,15 @@ def simulate_transmission(users: User, duration: int, rate, output_mode, mode):
                     print(f'User {user.id} , waited {user.slots} slots')
 
             for user in users:
+                # 送信したUserのslotsを再生成
                 if user.id == min_user.id:
                     user.reset_slots()
 
+                # 送信していないUserのslotsから最小slotsを引く
                 else:
                     user.slots -= min_slots
 
             # print([(user.id, user.num_re_trans, user.slots) for user in users])
-
-    # for user in users:
-    #     average_transmission_rate = user.data_transmitted / duration / 10**6
-    #     print(f"User {user.id} transmitted {user.num_transmitted} times, total data transmitted: {user.data_transmitted} bits, average transmission rate: {average_transmission_rate:.3f} Mbps")
 
     if output_mode != PRINT_MODE[3]:
         print("\nSimulation ended. Results:")
@@ -219,6 +228,7 @@ def simulate_transmission(users: User, duration: int, rate, output_mode, mode):
 
 
 if __name__ == "__main__":
+    # python main.pyで実行すると以下が実行される
     n = 80
 
     users = create_users(n)
